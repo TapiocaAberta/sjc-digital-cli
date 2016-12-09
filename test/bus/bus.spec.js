34885574ec3a9a0a39ea71b9251082fad79ce680
Paulo Luan <pauloluan.inova@gmail.com>
2016-12-09 13:33:30 -0200

    added extraction for url

--- test/bus/bus.spec.js
+++ test/bus/bus.spec.js
@@ -8,17 +8,23 @@ describe('transformCSVArrayToObject', function () {
     var mockData = [
       ['linha', 1, 2],
       ['nome', 'putim', 'sao judas'],
-      ['sentido', 'centro', 'bairro']
+      ['sentido', 'centro', 'bairro'],
+      ['horarios', '<a href="/secretarias/transportes/horario-e-itinerario.aspx?acao=d&amp;id_linha=1"><img src="http://servicos.sjc.sp.gov.br/imagens/consultar1.png" border="0"></a>', '<a href="/secretarias/transportes/horario-e-itinerario.aspx?acao=d&amp;id_linha=2"><img src="http://servicos.sjc.sp.gov.br/imagens/consultar1.png" border="0"></a>'],
+      ['rota', '<a href="/secretarias/transportes/horario-e-itinerario.aspx?acao=m&amp;id_linha=1"><img src="http://servicos.sjc.sp.gov.br/imagens/mapa.png" border="0"></a>', '<a href="/secretarias/transportes/horario-e-itinerario.aspx?acao=m&amp;id_linha=2"><img src="http://servicos.sjc.sp.gov.br/imagens/mapa.png" border="0"></a>']
     ]
 
     var expectedResult = [{
       line: 1,
       name: 'putim',
-      direction: 'centro'
+      direction: 'centro',
+      busSchedule: "/secretarias/transportes/horario-e-itinerario.aspx?acao=d&id_linha=1",
+      route: "/secretarias/transportes/horario-e-itinerario.aspx?acao=m&id_linha=1"
     }, {
       line: 2,
       name: 'sao judas',
-      direction: 'bairro'
+      direction: 'bairro',
+      busSchedule: "/secretarias/transportes/horario-e-itinerario.aspx?acao=d&id_linha=2",
+      route: "/secretarias/transportes/horario-e-itinerario.aspx?acao=m&id_linha=2"
     }]
 
     var result = bus.transformCSVArrayToObject(mockData)
