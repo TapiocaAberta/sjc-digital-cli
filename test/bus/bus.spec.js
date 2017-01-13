190930a897278463eb9b53709370027655e46f08
Paulo Luan <pauloluan.inova@gmail.com>
2017-01-13 16:47:50 -0200

    broken commit

--- test/bus/bus.spec.js
+++ test/bus/bus.spec.js
@@ -1,60 +1,81 @@
 // const chai = require('chai')
 // const expect = chai.expect
+
+require('co-mocha')
+
 const busModule = require('../../lib/bus')
 const assert = require('assert')
-const coMocha = require('co-mocha')
+const nock = require('nock')
 const cheerio = require('cheerio')
 const cheerioTableparser = require('cheerio-tableparser')
 
-let rawEntirePage = require('fs').readFileSync('test/bus/details_page_example.txt', 'utf8')
-
-var nock = require('nock')
+let rawHomePage = require('fs').readFileSync('test/bus/details_page_example.txt', 'utf8')
+let rawDetailsPage = require('fs').readFileSync('test/bus/details_page_example.txt', 'utf8')
 
 describe('transformCSVArrayToObject', function () {
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+
+  it('getPage - should get a nock page with success', function* () {
+    // mock the http request for the url and send the static page as response simulating the real page
+    nock('http://www.sjc.sp.gov.br')
+      .get('/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=96')
+      .reply(200, rawDetailsPage)
+
+    let $ = yield busModule.getPage('http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=96')
+    var title = $('head > title').text()
+
+    let expectedTitle = '\r\n  Horários e Itinerários de Ônibus\r\n        - Prefeitura Municipal de São José dos Campos\r\n      \r\n'
+    assert.deepEqual(title, expectedTitle)
+  })
+
   it('should transform the plain array with the first property as the key and the rest as value', function () {
     let rawList = [
-      ['linha', 1, 2],
-      ['nome', 'putim', 'sao judas'],
+      ['linha', 212, 214],
+      ['nome', 'PUTIM / TERMINAL CENTRAL – VIA AV. DOS ASTRONAUTAS (CIRCULAR NO CENTRO) O.S.O. 60', 'VILA TESOURO / AV. ENG. FRANCISCO JOSE LONGO (VIA SEBASTIAO GUALBERTO) (CIRCULAR NO CENTRO) O.S.O. 25'],
       ['sentido', 'centro', 'bairro'],
       ['horarios', '<a href="/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&amp;id_linha=1"><img src="http://servicos.sjc.sp.gov.br/imagens/consultar1.png" border="0"></a>', '<a href="/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&amp;id_linha=2"><img src="http://servicos.sjc.sp.gov.br/imagens/consultar1.png" border="0"></a>'],
       ['rota', '<a href="/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&amp;id_linha=1"><img src="http://servicos.sjc.sp.gov.br/imagens/mapa.png" border="0"></a>', '<a href="/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&amp;id_linha=2"><img src="http://servicos.sjc.sp.gov.br/imagens/mapa.png" border="0"></a>']
     ]
 
     let parsedList = [{
-      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=1',
-      'direction': 'centro',
-      'line': 1,
-      'name': 'putim',
-      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=1'
+      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=96',
+      'direction': 'PUTIM / TERMINAL CENTRAL',
+      'line': 212,
+      'name': 'PUTIM / TERMINAL CENTRAL – VIA AV. DOS ASTRONAUTAS (CIRCULAR NO CENTRO) O.S.O. 60 / TERMINAL CENTRAL – VIA AV. DOS ASTRONAUTAS (CIRCULAR NO CENTRO) O.S.O. 60',
+      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=96'
     }, {
-      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=2',
-      'direction': 'bairro',
-      'line': 2,
-      'name': 'sao judas',
-      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=2'
+      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=98',
+      'direction': 'VILA TESOURO / JOSE LONGO',
+      'line': 214,
+      'name': 'VILA TESOURO / AV. ENG. FRANCISCO JOSE LONGO (VIA SEBASTIAO GUALBERTO) (CIRCULAR NO CENTRO) O.S.O. 25',
+      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=98'
     }]
 
     let result = busModule.transformCSVArrayToObject(rawList)
     assert.deepEqual(result, parsedList)
   })
 
-  it.skip('parseBuses - should parse and return', function* () {
+  it.only('parseBuses - should parse and return', function* () {
     let buses = [{
-      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=1',
-      'direction': 'centro',
-      'line': 1,
-      'name': 'putim',
-      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=1'
+      'busSchedule': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=96',
+      'direction': 'PUTIM / TERMINAL CENTRAL',
+      'line': 212,
+      'name': 'PUTIM / TERMINAL CENTRAL – VIA AV. DOS ASTRONAUTAS (CIRCULAR NO CENTRO) O.S.O. 60 / TERMINAL CENTRAL – VIA AV. DOS ASTRONAUTAS (CIRCULAR NO CENTRO) O.S.O. 60',
+      'route': 'http://www.sjc.sp.gov.br/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=m&id_linha=96'
     }]
-
-    // TODO: add nock in other to simulate http requests
+    // mock the http request for the url and send the static page as response simulating the real page
+    nock('http://www.sjc.sp.gov.br')
+      .get('/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=96')
+      .reply(200, rawDetailsPage)
 
     let result = yield busModule.parseBuses(buses)
-    assert.deepEqual(result, buses)
+    assert.deepEqual(result, {})
   })
 
   it.skip('getScheduleTimes - should parse the page and return the schedule of buses', function* () {
-    let cheerioEntirePage = cheerio.load(rawEntirePage)
+    let cheerioEntirePage = cheerio.load(rawDetailsPage)
     cheerioTableparser(cheerioEntirePage)
 
     let result = yield busModule.parseDetailsPage(cheerioEntirePage)
@@ -103,10 +124,3 @@ describe('transformCSVArrayToObject', function () {
     assert.deepEqual(result, expectedDawn)
   })
 })
-
-function nockSJCWebSite () {
-  let responseJsonObject = '' // read it from FS
-  var couchdb = nock('http://www.sjc.sp.gov.br')
-    .get('/secretarias/mobilidade_urbana/horario-e-itinerario.aspx?acao=d&id_linha=1')
-    .reply(200, responseJsonObject)
-}
