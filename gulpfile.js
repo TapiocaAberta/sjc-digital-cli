1c3f5baf244b551dfcc92417bee930679e495d5c
Paulo Luan <pauloluan.inova@gmail.com>
2017-02-09 08:30:59 -0200

    added some more tests...

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,109 @@
-var path = require('path')
 var gulp = require('gulp')
-var standard = require('gulp-standard')
-var excludeGitignore = require('gulp-exclude-gitignore')
-var mocha = require('gulp-mocha')
-var istanbul = require('gulp-istanbul')
-var nsp = require('gulp-nsp')
-var plumber = require('gulp-plumber')
-// var coveralls = require('gulp-coveralls')
 var codecov = require('gulp-codecov')
-var babel = require('gulp-babel')
-var del = require('del')
-var isparta = require('isparta')
+var istanbul = require('gulp-istanbul')
+// We'll use mocha in this example, but any test framework will work
+var mocha = require('gulp-mocha')
 
-// Initialize the babel transpiler so ES2015 files gets compiled
-// when they're loaded
-require('babel-register')
+// var path = require('path')
+// var standard = require('gulp-standard')
+// var excludeGitignore = require('gulp-exclude-gitignore')
+// var mocha = require('gulp-mocha')
+// var istanbul = require('gulp-istanbul')
+// var nsp = require('gulp-nsp')
+// var plumber = require('gulp-plumber')
+// // var coveralls = require('gulp-coveralls')
+// var babel = require('gulp-babel')
+// var del = require('del')
+// var isparta = require('isparta')
 
-gulp.task('static', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(standard())
-    .pipe(standard.reporter('default', {
-      breakOnError: false,
-      quiet: true
-    }))
-})
+// // Initialize the babel transpiler so ES2015 files gets compiled
+// // when they're loaded
+// require('babel-register')
 
-gulp.task('nsp', function (cb) {
-  nsp({
-    package: path.resolve('package.json')
-  }, cb)
-})
+// gulp.task('static', function () {
+//   return gulp.src('lib/**/*.js')
+//     .pipe(excludeGitignore())
+//     .pipe(standard())
+//     .pipe(standard.reporter('default', {
+//       breakOnError: false,
+//       quiet: true
+//     }))
+// })
+
+// gulp.task('nsp', function (cb) {
+//   nsp({
+//     package: path.resolve('package.json')
+//   }, cb)
+// })
+
+// gulp.task('pre-test', function () {
+//   return gulp.src('lib/**/*.js')
+//     .pipe(excludeGitignore())
+//     .pipe(istanbul({
+//       includeUntested: true,
+//       instrumenter: isparta.Instrumenter
+//     }))
+//     .pipe(istanbul.hookRequire())
+// })
+
+// gulp.task('test', ['pre-test'], function (cb) {
+//   var mochaErr
+
+//   gulp.src('test/**/*.js')
+//     .pipe(plumber())
+//     .pipe(mocha({
+//       reporter: 'spec'
+//     }))
+//     .on('error', function (err) {
+//       mochaErr = err
+//     })
+//     .pipe(istanbul.writeReports())
+//     .on('end', function () {
+//       cb(mochaErr)
+//     })
+// })
+
+// gulp.task('watch', function () {
+//   gulp.watch(['lib/**/*.js', 'test/**'], ['test'])
+// })
+
+// gulp.task('codecov', ['test'], function () {
+//   return gulp.src('./coverage/lcov.info')
+//     .pipe(codecov())
+// })
+
+// gulp.task('babel', ['clean'], function () {
+//   return gulp.src('lib/**/*.js')
+//     .pipe(babel())
+//     .pipe(gulp.dest('dist'))
+// })
+
+// gulp.task('clean', function () {
+//   return del('dist')
+// })
+
+// gulp.task('prepublish', ['nsp', 'babel'])
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true,
-      instrumenter: isparta.Instrumenter
-    }))
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
     .pipe(istanbul.hookRequire())
 })
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({
-      reporter: 'spec'
-    }))
-    .on('error', function (err) {
-      mochaErr = err
-    })
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(['test/**/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran
     .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr)
-    })
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
 })
 
-gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test'])
-})
-
-gulp.task('codecov', ['test'], function () {
+gulp.task('codecov', function () {
   return gulp.src('./coverage/lcov.info')
     .pipe(codecov())
 })
 
-gulp.task('babel', ['clean'], function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('clean', function () {
-  return del('dist')
-})
-
-gulp.task('prepublish', ['nsp', 'babel'])
-gulp.task('default', ['static', 'test', 'codecov'])
+gulp.task('default', ['test', 'codecov'])
